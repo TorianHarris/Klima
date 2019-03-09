53c3c3f62932ce770d42c0a06a02eccd47217667
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-03-09 13:44:14 -0500

    added user validation and post routes for USer Sign-in and Sign-up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,47 @@
 var db = require("../models");
 
-module.exports = function (app) {
-  // Get all examples
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function(app) {
+  // userSignIn
+  app.post("/signin", (req, res) => {
+    let isEmpty = false;
+    let body = req.body;
+
+    for (x in body) {
+      if (body[x] === "") {
+        isEmpty = true;
+        console.log("body is empty");
+      }
+    }
+    if (!isEmpty) {
+      db.User.findAll({
+        where: {
+          email: req.body.email
+        }
+      })
+        .then(user => {
+          if (req.body.password !== user.password) {
+            return res.status().send("Invalid password");
+          }
+          res.status("").json(dbExamples);
+        })
+        .catch(error => {
+          res.send(error);
+        });
+    }
   });
 
-  // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-      res.json(dbExample);
+  //USER sign-up
+  app.post("/signup", (req, res) => {
+    db.User.create(req.body).then(newUserData => {
+      res.send("New user Created");
     });
   });
-
   // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
