0d4cc8cfc358955633fc5c754fe75844411aa39f
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-03-10 13:30:42 -0400

    added post routes

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE climateRisk;
+select * from Users; 
+select * from Comments;
+select * from Favorites;
+
+INSERT INTO Users (email,password,firstName,lastName)
+VALUES ("Kobe@Bryant.com", "LakersGold4Lyfe", "Kobe", "Bryant"),
+("Arnold@Govinator.gov", "I'llBeBack2020", "Arnold","Schwartisomething"),
+("Random@Anon.com","1234567","Not","Real");
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,18 +10,19 @@ module.exports = function(app) {
     for (x in body) {
       if (body[x] === "") {
         isEmpty = true;
-        console.log("body is empty");
+        res.send("All Fields Must Be Filled");
       }
     }
     if (!isEmpty) {
-      db.User.findAll({
+      db.User.findOne({
         where: {
           email: req.body.email
         }
       })
         .then(user => {
           if (req.body.password !== user.password) {
-            return res.status().send("Invalid password");
+            return res.send("Invalid password");
+            //TODO fidure out status code
           }
           res.status(200).json(user);
         })
@@ -37,6 +38,19 @@ module.exports = function(app) {
       res.json(newUserData);
     });
   });
+
+  app.post("/comment", (req, res) => {
+    db.Comment.create(req.body).then(newUserData => {
+      res.json(newUserData);
+    });
+  });
+
+  app.post("/favorite", (req, res) => {
+    db.Favorite.create(req.body).then(newUserData => {
+      res.json(newUserData);
+    });
+  });
+
   // Delete an example by id
   // app.delete("/api/examples/:id", function(req, res) {
   //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
