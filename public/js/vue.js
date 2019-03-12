8e8c5bdaff626358fbdfd1ea0e7395ecd730e700
JC <thompsonrell@gmail.com>
2019-03-12 19:29:31 -0400

    commit vue

--- public/js/vue.js
+++ public/js/vue.js
@@ -1,9 +1,9 @@
 var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-        message: 'Hello Vue!'
+        message: "Hello Vue!"
     }
-})
+});
 
 new Vue({
     el: "#jumbotron",
@@ -12,16 +12,14 @@ new Vue({
         For the Areas You Want to See.`,
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
-})
+});
 
 new Vue({
     el: "#forms",
-    data: {
-
-    },
+    data: {},
     methods: {
         sayHi: function () {
-            alert("hello")
+            alert("hello");
         }
     }
-})
+});
