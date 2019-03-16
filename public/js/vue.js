095947a69c93d69ae55866a9da017841d278a775
Bente Gber <benjamingreenberg@Benjamins-MBP-2.attlocal.net>
2019-03-15 23:18:53 -0400

    m

--- public/js/vue.js
+++ public/js/vue.js
@@ -4,7 +4,7 @@ new Vue({
     header: `Climate Warnings.
         For the Areas You Want to See.`,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      "The global climate system will continue to change for centuries because of both past and future emissions of greenhouse gases. Heat waves, floods and other impacts on ecosystems, human health and economy are likely to become more severe in the coming decades.This App is designed to help you understnad what areas are at the highest risk."
   }
 });
 
